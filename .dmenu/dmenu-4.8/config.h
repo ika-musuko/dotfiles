0436e9a0daeb71533c3f01fab4ec42ca7706ee0d
Sherwyn Sen <sherwyn.sen@gmail.com>
2020-02-02 13:33:11 -0800

    push before i artix-ify my colors

--- .dmenu/dmenu-4.8/config.h
+++ .dmenu/dmenu-4.8/config.h
@@ -4,13 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"ProFontWindows Nerd Font Mono:size=9"
+	"Cica:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#C397D8", "#111111" },
-	[SchemeSel] = { "#111111", "#1ABC9C" },
+                    /*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#00ff00", "#005577" },
+	[SchemeNormHighlight] = { "#00ff00", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
